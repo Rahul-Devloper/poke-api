aee22339d5bf6cf6bca935223a48cfe385f9f2fb
Rahul-Devloper <rahulramesh26@yahoo.com>
2021-07-10 12:11:13 +0530

    fetched api data

--- src/Components/Pokemon.js
+++ src/Components/Pokemon.js
@@ -2,47 +2,50 @@ import {React, useState, useEffect} from 'react'
 
 
     function Pokemon() {
-        const [error, setError] = useState(null);
-        const [isLoaded, setIsLoaded] = useState(false);
-        const [items, setItems] = useState(['Poke Api']);
+        // const [error, setError] = useState(null);
+        // const [isLoaded, setIsLoaded] = useState(false);
+         const [items, setItems] = useState([]);
       
-        // Note: the empty deps array [] means
-        // this useEffect will run once
-        // similar to componentDidMount()
-        useEffect(() => {
-          fetch("https://pokeapi.co/api/v2/berry/cheri")
-            .then(res => res.json())
-            .then(
-              (result) => {
-                setIsLoaded(true);
-                setItems(result.items);
-              },
-              // Note: it's important to handle errors here
-              // instead of a catch() block so that we don't swallow
-              // exceptions from actual bugs in components.
-              (error) => {
-                setIsLoaded(true);
-                setError(error);
-              }
-            )
-        }, [])
+  
+        // useEffect(() => {
+        //   fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+        //     .then(
+        //       (result) => {
+        //         setIsLoaded(true);
+        //         setItems(result.items);
+        //       },
+            
+        //       (error) => {
+        //         setIsLoaded(true);
+        //         setError(error);
+        //       }
+        //     )
+        // }, [])
+
+
       
-        if (error) {
-          return <div>Error: {error.message}</div>;
-        } else if (!isLoaded) {
-          return <div>Loading...</div>;
-        } else {
+        // if (error) {
+        //   return <div>Error: {error.message}</div>;
+        // } else if (!isLoaded) {
+        //   return <div>Loading...</div>;
+        // } else {
+
+
+        fetch('https://pokeapi.co/api/v2/pokemon/')
+        .then(res=>res.json())
+        .then(
+          (data)=>{
+            setItems(data.results)
+          }
+        )
           return (
             <ul>
-              {items.map(item => (
-                <li key={item.id}>
-                  Smoothness: {item.smoothness},
-                  Size: {item.size}
-                </li>
+              {items.map((p)=>
+              (
+                <li>Name: {p.name}</li>
               ))}
             </ul>
           );
         }
-      }
 
 export default Pokemon
