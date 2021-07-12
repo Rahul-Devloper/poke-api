0803029a79639869abffa65274521233f8184532
Rahul-Devloper <rahulramesh26@yahoo.com>
2021-07-13 01:30:38 +0530

    attained the get request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 import Pokemon from './Components/Pokemon';
 
+
 function App() {
+
+const [pokemon, setPokemon]=useState([])
+
+const pokemonApi = ()=>{
+  axios.get('https://pokeapi.co/api/v2/pokemon/')
+  .then((response)=>{
+    let data = response.data.results;
+    setPokemon(data);
+  })
+  
+}
+
+
+
   return (
     <div className="App">
-      <h1>Poke API</h1>
-      <Pokemon />
+      <h2>Poke Api</h2>
+      
+      <Pokemon pokemon = {pokemon} pokemonApi = {pokemonApi}/>
     </div>
   );
 }
--- src/Components/Pokemon.js
+++ src/Components/Pokemon.js
@@ -1,51 +1,15 @@
-import {React, useState, useEffect} from 'react'
+import React from 'react'
+import './Pokemon.css'
 
-
-    function Pokemon() {
-        // const [error, setError] = useState(null);
-        // const [isLoaded, setIsLoaded] = useState(false);
-         const [items, setItems] = useState([]);
-      
-  
-        // useEffect(() => {
-        //   fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-        //     .then(
-        //       (result) => {
-        //         setIsLoaded(true);
-        //         setItems(result.items);
-        //       },
-            
-        //       (error) => {
-        //         setIsLoaded(true);
-        //         setError(error);
-        //       }
-        //     )
-        // }, [])
-
-
-      
-        // if (error) {
-        //   return <div>Error: {error.message}</div>;
-        // } else if (!isLoaded) {
-        //   return <div>Loading...</div>;
-        // } else {
-
-
-        fetch('https://pokeapi.co/api/v2/pokemon/')
-        .then(res=>res.json())
-        .then(
-          (data)=>{
-            setItems(data.results)
-          }
-        )
-          return (
-            <ul>
-              {items.map((p)=>
-              (
-                <li>Name: {p.name}</li>
-              ))}
-            </ul>
-          );
-        }
+const Pokemon = ({pokemon, pokemonApi}) => {
+  return (
+    <div className = 'pokemon'>
+      <button onClick = {pokemonApi}>Click Here</button>
+      {pokemon.map((poke) => (
+      <li className = 'list'>{poke.name}</li>
+      ))}
+    </div>
+  )
+}
 
 export default Pokemon
