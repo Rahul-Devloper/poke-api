8089bb070feeed69cbcaf69e2770b7cff20ab800
Rahul-Devloper <rahulramesh26@yahoo.com>
2021-07-17 23:54:43 +0530

    finished the pokemon.js page with mock data

--- src/Components/Pokedex.js
+++ src/Components/Pokedex.js
@@ -15,12 +15,16 @@ const Pokedex = (props) => {
   const [pokemonData, setPokemonData] = useState(Mockdata);
   const getPokemonCard = (pokemonId) => {
     console.log(pokemonData[`${pokemonId}`]);
-    const{id,name}= pokemonData[`${pokemonId}`]
-    const sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
-    const {history} = props
+    const { id, name } = pokemonData[`${pokemonId}`];
+    const sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+    const { history } = props;
     return (
       <Col xs={12} sm={6} md={4} key={pokemonId}>
-        <Card style={{ width: "18rem" }} className="m-2" onClick={()=>history.push(`${pokemonId}`)} >
+        <Card
+          style={{ width: "18rem", textTransform:'capitalize' }}
+          className="m-2"
+          onClick={() => history.push(`${pokemonId}`)}
+        >
           <Card.Img variant="top" src={sprite} />
           <Card.Body>
             <Card.Title>{`${id}. ${name}`}</Card.Title>
@@ -35,11 +39,12 @@ const Pokedex = (props) => {
     <>
       <Navbar bg="primary" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+          <Navbar.Brand href="#home">Pokedex</Navbar.Brand>
         </Container>
       </Navbar>
       {pokemonData ? (
         <Container>
+          <h1>Welcome to PokeDex</h1>
           <Row>
             {Object.keys(pokemonData).map((pokemonId) => {
               return getPokemonCard(pokemonId);
@@ -51,7 +56,6 @@ const Pokedex = (props) => {
           <span className="visually-hidden">Loading...</span>
         </Spinner>
       )}
-      <h1>Welcome to PokeDex</h1>
     </>
   );
 };
--- src/Components/Pokemon.js
+++ src/Components/Pokemon.js
@@ -1,21 +1,52 @@
-import {React, useState} from 'react'
-import { Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios'
-import './Pokemon.css'
-import { useParams } from 'react-router-dom';
+import { React, useState } from "react";
+import { Button, Container, Navbar } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import "./Pokemon.css";
+import { useParams } from "react-router-dom";
+import Mockdata from "./Mockdata";
 
-const Pokemon = () => {
+const Pokemon = (props) => {
+  const { pokemonId } = useParams();
+  const [pokemon, setPokemon] = useState(Mockdata[`${pokemonId}`]);
 
-  const {pokemonId} = useParams()
+  const generatePokemonJsx = (pokemon) => {
+    const { name, id, species, height, weight, types, sprites } = pokemon;
+    const fullImageUrl = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
+    const { front_default } = sprites;
+    return (
+      <>
+        <Container style={{textAlign:'center'}}>
+        <h1 style={{textTransform:"Capitalize"}}>
+          {`${id}. ${name}`}
+          <img src={front_default} alt="" />
+        </h1>
+        <img src={fullImageUrl} alt="" style={{height:'300px', width:'300px'}} />
+        <h3>Pokemon Stats</h3>
+        <h4>Species:{species.name}</h4>
+        <h4>Height:{height}</h4>
+        <h4>Weight:{weight}</h4>
+        <h4>Types: {types.slot}
+          {types.map((type)=>{
+            console.log(type.type.name)
+            return <span>{type.type.name}</span>
+          })}</h4>
+        </Container>
+      </>
+    );
+  };
 
-
-  
   return (
-    <div>
-      <h1>{`Welcome to Pokemon ${pokemonId}`}</h1>
+    <div style={{textTransform:'capitalize', textAlign:'center'}}>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Pokedex</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <h1>{`Welcome to ${pokemon.name} stats page`}</h1>
+      {generatePokemonJsx(pokemon)}
     </div>
-  )
-}
+  );
+};
 
-export default Pokemon
+export default Pokemon;
